208739bbc757b3e322cb89c7f6b76392d6310d13
JaydevKumarSingh <kumarsinghjaydev@gmail.com>
2020-07-03 17:55:06 +0530

    Adding Create Tabel

---
+++ src/com/sandeep/json/data/servlet/CreateTable.java
@@ -0,0 +1,10 @@
+package com.sandeep.json.data.servlet;
+
+public class CreateTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello World");
+	}
+
+}
